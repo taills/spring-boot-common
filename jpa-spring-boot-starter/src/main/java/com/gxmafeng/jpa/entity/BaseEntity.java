9d2159c3f853e0f2e30d47b79bd9505500f6c4d8
nil <taills@qq.com>
2021-10-20 17:07:55 +0800

    修改 JPA 配置

--- jpa-spring-boot-starter/src/main/java/com/gxmafeng/jpa/entity/BaseEntity.java
+++ jpa-spring-boot-starter/src/main/java/com/gxmafeng/jpa/entity/BaseEntity.java
@@ -1,9 +1,11 @@
 package com.gxmafeng.jpa.entity;
 
+import com.gxmafeng.service.common.utils.SnowFlake;
 import io.swagger.annotations.ApiModelProperty;
 
-import javax.persistence.*;
 import java.util.Date;
+
+import javax.persistence.*;
 
 /**
  * @ClassName BaseEntity
@@ -13,81 +15,81 @@ import java.util.Date;
  **/
 @MappedSuperclass
 public class BaseEntity {
+
     /**
      * 主键id
      */
-    @ApiModelProperty(value = "主键id")
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @ApiModelProperty(value = "主键id")
     private Long id;
-//
-//    /**
-//     * 创建时间
-//     * nullable : true
-//     * default  : CURRENT_TIMESTAMP
-//     */
-//    @ApiModelProperty(value = "创建时间")
-//    @Column(name = "gmt_create", nullable = true)
-//    private java.util.Date gmtCreate = new Date();
-//
-//    /**
-//     * 修改时间
-//     * nullable : true
-//     * default  : CURRENT_TIMESTAMP
-//     */
-//    @ApiModelProperty(value = "修改时间")
-//    @Column(name = "gmt_modified", nullable = true)
-//    private java.util.Date gmtModified;
-//
-//    /**
-//     * 删除时间，用于软删除，为空是有效，有值时表示已经删除
-//     * nullable : true
-//     * default  : null
-//     */
-//    @ApiModelProperty(value = "删除时间，用于软删除，为空是有效，有值时表示已经删除")
-//    @Column(name = "gmt_delete", nullable = true)
-//    private java.util.Date gmtDelete;
+
+
+    /**
+     * 创建时间
+     * nullable : true
+     * default  : CURRENT_TIMESTAMP
+     */
+    @ApiModelProperty(value = "创建时间")
+    @Column(name = "gmt_create", nullable = true)
+    private java.util.Date gmtCreate;
+
+    /**
+     * 修改时间
+     * nullable : true
+     * default  : CURRENT_TIMESTAMP
+     */
+    @ApiModelProperty(value = "修改时间")
+    @Column(name = "gmt_modified", nullable = true)
+    private java.util.Date gmtModified;
 
 
     public Long getId() {
-        return id;
+        return this.id;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
-//
-//    public Date getGmtCreate() {
-//        return gmtCreate;
-//    }
-//
-//    public void setGmtCreate(Date gmtCreate) {
-//        this.gmtCreate = gmtCreate;
-//    }
-//
-//    public Date getGmtModified() {
-//        return gmtModified;
-//    }
-//
-//    public void setGmtModified(Date gmtModified) {
-//        this.gmtModified = gmtModified;
-//    }
-//
-//    public Date getGmtDelete() {
-//        return gmtDelete;
-//    }
-//
-//    public void setGmtDelete(Date gmtDelete) {
-//        this.gmtDelete = gmtDelete;
-//    }
-//
-//    @Override
-//    public String toString() {
+
+    public Date getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Date gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public Date getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Date gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+
+    @Override
+    public String toString() {
 //        return "BaseEntity{" +
 //                "id=" + id +
 //                ", gmtCreate=" + gmtCreate +
 //                ", gmtModified=" + gmtModified +
-//                ", gmtDelete=" + gmtDelete +
 //                '}';
-//    }
+        return "{id: " + id + " }";
+    }
+
+    @PrePersist
+    void preInsert() {
+        if (this.gmtCreate == null) {
+            this.gmtCreate = new Date();
+        }
+        if (this.id == null) {
+            this.id = SnowFlake.get().nextId();
+        }
+    }
+
+    @PreUpdate
+    void preUpdate() {
+        this.gmtModified = new Date();
+    }
 }
