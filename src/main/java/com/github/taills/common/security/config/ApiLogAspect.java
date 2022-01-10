0a68ca4c3e09c2acaa277c714048310f41153cee
nil <taills@qq.com>
2022-01-10 13:04:49 +0800

    Update ApiLogAspect.java
    修改切面配置参数及日志输出格式

--- src/main/java/com/github/taills/common/security/config/ApiLogAspect.java
+++ src/main/java/com/github/taills/common/security/config/ApiLogAspect.java
@@ -1,5 +1,6 @@
 package com.github.taills.common.security.config;
 
+import com.github.taills.common.util.SnowFlake;
 import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
@@ -10,6 +11,7 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 /**
  * @ClassName ApiLogAspect
@@ -22,7 +24,9 @@ import javax.servlet.http.HttpServletRequest;
 @Slf4j
 public class ApiLogAspect {
 
-    @Pointcut("@within(org.springframework.web.bind.annotation.ResponseBody)")
+    private final String RequestIdHeaderName = "RequestId";
+
+    @Pointcut("@within(com.github.taills.common.annotation.ApiResponseBody) || this(com.github.taills.common.controller.BaseController)")
     public void apiLog() {
     }
 
@@ -32,30 +36,32 @@ public class ApiLogAspect {
     public void doBefore(JoinPoint joinPoint) {
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
-        log.info("========================================== Start ==========================================");
-        log.info("URL            : {}", request.getRequestURL().toString());
-        log.info("HTTP Method    : {}", request.getMethod());
-        log.info("Class Method   : {}.{}", joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
-        log.info("IP             : {}", request.getRemoteAddr());
-        if (request.getUserPrincipal() != null) {
-            log.info("User           : {}", request.getUserPrincipal().getName());
+        if (null == request.getAttribute(RequestIdHeaderName)) {
+            request.setAttribute(RequestIdHeaderName, SnowFlake.get().nextSid());
         }
-        log.info("Request Args   : {}", GSON.toJson(joinPoint.getArgs()));
+        log.info("========================================== {} ==========================================", request.getAttribute(RequestIdHeaderName));
+        log.info("{}\tURL\t\t\t\t\t{}", request.getAttribute(RequestIdHeaderName), request.getRequestURL().toString());
+        log.info("{}\tHTTP Method\t\t\t{}", request.getAttribute(RequestIdHeaderName), request.getMethod());
+        log.info("{}\tClass Method\t\t{}.{}", request.getAttribute(RequestIdHeaderName), joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
+        log.info("{}\tIP\t\t\t\t\t{}", request.getAttribute(RequestIdHeaderName), request.getRemoteAddr());
+        if (request.getUserPrincipal() != null) {
+            log.info("{}\tUser\t\t\t\t{}", request.getAttribute(RequestIdHeaderName), request.getUserPrincipal().getName());
+        }
+        log.info("{}\tRequest Args\t\t{}", request.getAttribute(RequestIdHeaderName), GSON.toJson(joinPoint.getArgs()));
     }
 
     @After("apiLog()")
     public void doAfter() {
-        log.info("=========================================== End ===========================================");
-
-        log.info("");
     }
 
     @Around("apiLog()")
     public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
         long startTime = System.currentTimeMillis();
         Object result = proceedingJoinPoint.proceed();
-        log.info("Response Args  : {}", new Gson().toJson(result));
-        log.info("Time-Consuming : {} ms", System.currentTimeMillis() - startTime);
+        log.info("{}\tResponse Args\t\t{}", request.getAttribute(RequestIdHeaderName), new Gson().toJson(result));
+        log.info("{}\tTime-Consuming\t\t{} ms", request.getAttribute(RequestIdHeaderName), System.currentTimeMillis() - startTime);
         return result;
     }
 
