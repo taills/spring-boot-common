5189d4bb91984248ffb772e740dff41307997920
taills <taills@qq.com>
2022-01-13 19:54:27 +0800

    Update AuthenticationController.java
    为 注册管理员、登录接口增加限流配置

--- src/main/java/io/github/taills/common/security/controller/AuthenticationController.java
+++ src/main/java/io/github/taills/common/security/controller/AuthenticationController.java
@@ -4,6 +4,7 @@ import io.github.taills.common.annotation.ApiResponseBody;
 import io.github.taills.common.exception.ExceptionManager;
 import io.github.taills.common.jpa.entity.SecurityUser;
 import io.github.taills.common.jpa.service.SecurityUserService;
+import io.github.taills.common.limiter.Limit;
 import io.github.taills.common.response.ApiResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -33,6 +34,7 @@ public class AuthenticationController {
 
     @PostMapping("/login")
     @ApiOperation("登录")
+    @Limit(permitsPerSecond = 0.5)
     public ApiResult login(@RequestParam("username") String username, @RequestParam("password") String password) {
         Optional<SecurityUser> optional;
         optional = this.userService.verifyUsernameAndPassword(username, password);
@@ -44,8 +46,9 @@ public class AuthenticationController {
     }
 
     @PostMapping("/admin/register")
-    @ApiOperation(value = "注册管理员账号",notes = "仅在数据库为空时可以使用")
+    @ApiOperation(value = "注册管理员账号", notes = "仅在数据库为空时可以使用")
     @Transactional
+    @Limit(permitsPerSecond = 0.1)
     public ApiResult registerAdmin(@RequestParam("username") String username, @RequestParam("password") String password) {
         if (userService.count() > 0) {
             throw ExceptionManager.create(1002);
