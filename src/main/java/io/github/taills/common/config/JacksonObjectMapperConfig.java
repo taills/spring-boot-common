820c0bf5a762caa723f02697646a3a040a0d2153
taills <taills@qq.com>
2022-01-13 12:52:14 +0800

    Update JacksonObjectMapperConfig.java
    修改 ObjectMapper 一些配置

--- src/main/java/io/github/taills/common/config/JacksonObjectMapperConfig.java
+++ src/main/java/io/github/taills/common/config/JacksonObjectMapperConfig.java
@@ -1,10 +1,11 @@
 package io.github.taills.common.config;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -12,6 +13,8 @@ import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @ClassName JsonConfig
@@ -25,16 +28,25 @@ public class JacksonObjectMapperConfig {
     @Bean
     @Primary
     public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
-        final JavaTimeModule simpleModule = new JavaTimeModule();
-        //序列化将Long转String类型
-        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
-        simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        //针对于JDK新时间类。序列化时带有T的问题，自定义格式化字符串
+        final JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+
         SimpleModule bigIntegerModule = new SimpleModule();
         //序列化将BigInteger转String类型
         bigIntegerModule.addSerializer(BigInteger.class, ToStringSerializer.instance);
         SimpleModule bigDecimalModule = new SimpleModule();
         //序列化将BigDecimal转String类型
         bigDecimalModule.addSerializer(BigDecimal.class, ToStringSerializer.instance);
-        return builder.createXmlMapper(false).modulesToInstall(simpleModule).build();
+
+        return builder.createXmlMapper(false)
+                .modulesToInstall(javaTimeModule)
+                .modulesToInstall(bigIntegerModule)
+                .modulesToInstall(bigDecimalModule)
+                .failOnEmptyBeans(false)
+                //针对于Date类型，文本格式化
+                .simpleDateFormat("yyyy-MM-dd HH:mm:ss")
+                .build();
     }
 }
