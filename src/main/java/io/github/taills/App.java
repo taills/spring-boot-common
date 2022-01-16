08d2ee05581c8c97e404d63b5a06f97275dcbc26
taills <taills@qq.com>
2022-01-16 23:35:38 +0800

    增加测试的入口函数

---
+++ src/main/java/io/github/taills/App.java
@@ -0,0 +1,18 @@
+package io.github.taills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author nil
+ */
+@SpringBootApplication
+@EnableScheduling
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/taills/AppTest.java
@@ -0,0 +1,41 @@
+package io.github.taills;
+
+import org.junit.FixMethodOrder;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+class AppTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Test
+    void setp1Register() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/security/admin/register")
+                        .accept(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                        .param("username", "admin")
+                        .param("password", "password")
+                ).andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+        ;
+    }
+
+}
