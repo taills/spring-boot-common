820f5e11ec26ed658a97f29146e70eeb2f8abecc
taills <taills@qq.com>
2022-01-18 15:01:52 +0800

    fix: swagger 页面 404

--- src/main/java/io/github/taills/common/security/config/CommonMvcConfig.java
+++ src/main/java/io/github/taills/common/security/config/CommonMvcConfig.java
@@ -4,16 +4,17 @@ import io.github.taills.common.security.interceptor.IPAclInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 /**
- * @ClassName IPAddressAclConfig
+ * @ClassName CommonMvcConfig
  * @Description
  * @Author nil
  * @Date 2022/1/18 12:30 AM
  **/
 @Configuration
-public class IPAclConfig extends WebMvcConfigurationSupport {
+public class CommonMvcConfig extends WebMvcConfigurationSupport {
 
     @Autowired
     private IPAclInterceptor ipAclInterceptor;
@@ -21,5 +22,18 @@ public class IPAclConfig extends WebMvcConfigurationSupport {
     protected void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(ipAclInterceptor).addPathPatterns("/**").excludePathPatterns("/public/**");
         super.addInterceptors(registry);
+    }
+
+    /**
+     * 这里重新配置 swagger 的资源，要不然页面 404
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(
+                "classpath:/static/");
+        registry.addResourceHandler("/swagger-ui/**").addResourceLocations(
+                "classpath:/META-INF/resources/webjars/springfox-swagger-ui/");
+        super.addResourceHandlers(registry);
     }
 }
